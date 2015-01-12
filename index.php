602485c792cc37b25d4ae3eab46e17a2b9c6477c
bcit <bcit@bcit-PC>
2015-01-11 16:45:51 -0800

    Added index.php file with "Hello, World"

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<!--
+  To change this license header, choose License Headers in Project Properties.
+  To change this template file, choose Tools | Templates
+  and open the template in the editor.
+-->
+
+<<html>
+    <head>
+        <meta charset="UTF-8"
+        <title>COMP4711 - Lab01</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello, World!";
+        ?>
+    </body>
+</html>
+
+
